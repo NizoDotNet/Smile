37d83229e9ce4c66754e5be2fb37b23716fedfe3
NizoDotNet <mmmdovnizami.official@gmail.com>
2025-01-07 21:16:16 +0400

    parallel

--- Smile/Program.cs
+++ Smile/Program.cs
@@ -1,28 +1,50 @@
 ﻿using Smile;
 using Smile.Model;
 using Smile.Parser;
+using System.Diagnostics;
 using System.Text;
 Console.OutputEncoding = Encoding.UTF8;
 
-
+Stopwatch stopwatch = new();
 Parser parser = new(new ConsoleLogger());
 
-var sts = await parser.ParseStudentsAsync(6358, 6388);
-//var sts = parser.ParseStudents(6358, 6388)
-//    .Select(s => new Student()
-//    {
-//        Name = s.Name,
-//        Subjects = s.Subjects.Where(c => c.Semester == "P-3").ToList(),
-//    })
-//    .ToList(); ;
+stopwatch.Start();
+
+
+
+
+var sts = parser.ParseStudents(6358, 6388);
 foreach (var student in sts)
 {
     Console.WriteLine(student.Name);
     foreach (var sub in student.Subjects)
     {
-        Console.WriteLine($"{sub.Name}: {sub.EnterScore} {sub.OutScore}");
+        //Console.WriteLine($"{sub.Name}: {sub.EnterScore} {sub.OutScore}");
     }
 }
+
+stopwatch.Stop();
+Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
+
+
+stopwatch.Restart();
+var tasks = new List<Task<Student>>();
+for (int i = 6358; i <= 6388; i++)
+{
+    tasks.Add(parser.ParseStudentAsync(i));
+}
+sts = await Task.WhenAll(tasks);
+foreach (var student in sts)
+{
+    //Console.WriteLine(student.Name);
+    foreach (var sub in student.Subjects)
+    {
+        //Console.WriteLine($"{sub.Name}: {sub.EnterScore} {sub.OutScore}");
+    }
+}
+stopwatch.Stop();
+
+Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
 
 //var a = AVG.GetAvgOfFinalScore(sts);
 //var arrA = a.OrderByDescending(c => c.AVG).ToArray();
